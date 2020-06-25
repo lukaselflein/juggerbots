fb4261db9250bdd46bc011f867d27b5bd86968a8
Lukas Elflein <lukaselflein@gmail.com>
2020-06-25 12:00:05 +0200

    poc for connecting to google

--- pompfbot.py
+++ pompfbot.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+    #!/usr/bin/env python
+    # -*- coding: utf-8 -*-
 # This program is dedicated to the public domain under the CC0 license.
 """
 Telegram bot for keeping the scores in a jugger team.
@@ -12,9 +12,13 @@ import time
 import os
 import re
 import pandas as pd
+import pickle
+import telegram
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-import telegram
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -32,7 +36,12 @@ def parse_cli():
 
 def start(update, context):
     """Send a message when the command /start is issued."""
-    update.message.reply_text('Hi Rigor!')
+    # Set up a Google Sheet document as a database
+    g_sheet = GoogleSheet(sheet_id='1Qmrj-YI5RokrKkfXf9wNubt2oezTGmb8bL-KCUyWf7o', 
+                          sheet_range='Data!A1:E')
+    context.bot_data['google_sheet'] = g_sheet
+    update.message.reply_text('Google Sheet initialized.')
+
 
 
 def help(update, context):
@@ -86,7 +95,7 @@ def handle_data(text, update, context):
     player_name, opponent_name = names[0], names[1]
     # Add a new data row like
     # 2020-20-6 max lu 4 10
-    data = [[pd.Timestamp.now(), player_name, opponent_name, 
+    data = [[time.time(), player_name, opponent_name, 
              player_points, opponent_points]]
     # Append a duplicate where player and opponent are swapped, for easier filtering, like
     # 2020-20-6 lu max 10 4
@@ -125,36 +134,50 @@ def parse(update, context):
 
         # Only use output if no error was encountered
         if df is not None:
-            data = save_entry(df)
+            data = save_entry(update, context, df)
 
     return
 
 
-def save_entry(df, path='./data/scores.csv'):
+def save_entry(update, context, df, path='./data/scores.csv'):
     """Append pandas dataframe to csv at rest."""
 
-    # Load old data
-    rest_data = read_db(path=path, update=None, context=None)
+    google_sheet = context.bot_data['google_sheet']
 
-    if rest_data is not None:
-        # If we have old data on disk, append the new data
-        new_data = pd.concat([rest_data, df], ignore_index=True)
+    if google_sheet is None:
+        # Load old data
+        rest_data = read_db(path=path, update=None, context=None)
+
+        if rest_data is not None:
+            # If we have old data on disk, append the new data
+            new_data = pd.concat([rest_data, df], ignore_index=True)
+        else:
+            # Otherwise, the new stuff is all we have 
+            new_data = df
+
+        new_data.to_csv(path, index=False)
+
     else:
-        # Otherwise, the new stuff is all we have 
-        new_data = df
+        google_sheet.append(df.values)
 
-    new_data.to_csv(path, index=False)
     return new_data
 
 
 def read_db(update=None, context=None, path='./data/scores.csv'):
     """Output the currently stored data as pandas dataframe"""
-    try:
-        # Check if we already have a file
-        rest_data = pd.read_csv(path)
-    except:
-        rest_data = None
-    return rest_data
+
+    google_sheet = context.bot_data['google_sheet']
+    # Local fallback
+    if google_sheet is None:
+        try:
+            # Check if we already have a file
+            data = pd.read_csv(path)
+        except:
+            data = None
+    # Google sheets database
+    else:
+        data = google_sheet.read()
+    return data
 
 
 def delete_db(update=None, context=None, path='./data/scores.csv'):
@@ -237,11 +260,88 @@ def error(update, context):
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 
+class GoogleSheet:
+    """A google spreadsheet."""
+    def __init__(self, sheet_id, sheet_range):
+        """Handle login and default parameters."""
+        creds = self.auth()
+        service = build('sheets', 'v4', credentials=creds)
+        self.sheet = service.spreadsheets()
+        self.sheet_id = sheet_id
+        self.sheet_range = sheet_range
+
+    def auth(self):
+        """Logs in to the Google API.
+        The `credentials.json` secret file stores login information.
+        Saves an access token in `token.pickle`."""
+
+        creds = None
+        # The file token.pickle stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists('token.pickle'):
+            with open('token.pickle', 'rb') as token:
+                creds = pickle.load(token)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json',  # Login Credentials
+                    ['https://www.googleapis.com/auth/drive.file'])  # read&write one file
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('token.pickle', 'wb') as token:
+                pickle.dump(creds, token)
+
+        return creds
+
+
+    def read(self):
+        result = self.sheet.values().get(spreadsheetId=self.sheet_id,
+                                    range=self.sheet_range).execute()
+        values = result.get('values', [])
+        return values
+
+
+    def write(self, values=None):
+        if values is None:
+            values = [
+                ['Datetime', 'Player', 'Opponent', 'Player Score', 'Opponent Score'],
+                ['1', 'lu', 'max', '10', '4'],
+                ['2', 'lu', 'max', '9', '10'],
+                ['3', 'test', 'test2', '1', '2'],
+                ['4', 'test2', 'test', '2', '1']
+            ]
+        body = { 'values': values}
+        result = self.sheet.values().update(spreadsheetId=self.sheet_id,
+                                       range=self.sheet_range,
+                                       valueInputOption='USER_ENTERED',
+                                       body=body).execute()
+        print(f'{result.get("updatedCells")} cells updated')
+
+
+    def append(self, values=None):
+        print(values)
+        exit()
+        if values is None:
+            values = [['10', 'appended', 'a2', '1', '3']]
+            values += [['10', 'a2', 'appended', '3', '1']]
+        initial_values = self.read()
+        # Append to the old data:
+        new_values = initial_values + values
+        self.write(values=new_values)
+    
+
 def main():
     """Start the bot."""
 
+
+    # Read Command line interface parameters
     keyname = parse_cli()
     
+    # Read Telegram secrets file
     token = read_secrets(path='./secrets.json', token_name=keyname)
 
     # Create the Updater and pass it your bot's token.
